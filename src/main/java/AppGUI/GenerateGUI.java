3b0f786e0ca2b80aaeff1758747cc78ff419f63c
XWing <89142321+XWing9@users.noreply.github.com>
2025-08-28 10:06:06 +0200

    JSON driven GUI gnereration

--- src/main/java/AppGUI/GenerateGUI.java
+++ src/main/java/AppGUI/GenerateGUI.java
@@ -2,18 +2,25 @@ package AppGUI;
 
 import AppLogic.AppManager;
 import AppLogic.JSONReader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
 
-import java.util.Arrays;
-
-public class GenerateGUI<JSONArray, JSONObject> {
+public class GenerateGUI extends GuiController{
 
     AppManager appManager;
-    JSONReader JSONReader;
+    JSONReader JSONReader = new JSONReader();
 
 
     public void GenerateStartingGUI(AppManager appManager) {
 
         String[] buttonsText = JSONReader.JSONReader("names");
+        
+        for (String buttonText : buttonsText) {
+            Button button = new Button(buttonText);
+            // Route clicks to existing handler in GuiController
+            button.setOnAction(this::StartingOrder);
+            container.getChildren().add(button);
+        }
 
         //create buttons through a for loop
     }
--- src/main/java/AppGUI/GuiController.java
+++ src/main/java/AppGUI/GuiController.java
@@ -1,6 +1,5 @@
 package AppGUI;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
