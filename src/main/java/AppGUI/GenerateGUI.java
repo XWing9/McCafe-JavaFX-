e110fd8909ce3d3caf9b2dba716062a60071a10d
XWing <89142321+XWing9@users.noreply.github.com>
2025-09-02 13:09:19 +0200

    made JSON GUI gen work

--- src/main/java/AppGUI/GenerateGUI.java
+++ src/main/java/AppGUI/GenerateGUI.java
@@ -1,18 +1,12 @@
 package AppGUI;
 
-import AppLogic.AppManager;
 import AppLogic.JSONReader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
-import javafx.fxml.FXML;
 
-public class GenerateGUI extends GuiController{
+public class GenerateGUI{
 
-    //remove fxml out of here and just call it from the gui controller?
-    AppManager appManager;
     JSONReader JSONReader = new JSONReader();
-    @FXML
-    private TilePane container;
 
     public void GenerateStartingGUI(TilePane container, GuiController controller) {
 
@@ -20,10 +14,10 @@ public class GenerateGUI extends GuiController{
 
         for (String buttonText : buttonsText) {
             Button button = new Button(buttonText);
-            // Route clicks to existing handler in GuiController
             button.setOnAction(controller::StartingOrder);
             container.getChildren().add(button);
         }
+
 
     }
 }
